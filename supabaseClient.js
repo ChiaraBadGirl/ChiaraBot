aee4e5b901ef50262d380c29db71a2301c370864
ChiaraBadGirl <schaulnoe@gmail.com>
2025-07-30 23:05:30 +0200

    SUPABASE V3.1

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://dailaqknciqwimozyvkk.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhaWxhcWtuY2lxd2ltb3p5dmtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzODM0NjcsImV4cCI6MjA2ODk1OTQ2N30.8Z4MhwKgUJ3mlnPf_kw8fVnfCRPVBRBSVWOnXK3AZsM';
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
